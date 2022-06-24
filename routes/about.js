6b0fe9601f9e27b296aad9ad2deadf193705b5bf
clementv <vermylenclement@gmail.com>
2022-06-24 23:24:56 +0200

    nodemon

--- routes/about.js
+++ routes/about.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const app = express();
 
-router.get('/', (req, res) => {
+router.get('/about', (req, res) => {
     res.render('about')
 });
 
